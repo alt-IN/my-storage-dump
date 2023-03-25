627f65e19d97e3d8d39b39681865fce63d271876
alt-IN <nikita135@gmail.com>
2023-03-25 23:59:13 +0300

    Make changes in table names

--- SQL_scripts/RequestDataR001.sql
+++ SQL_scripts/RequestDataR001.sql
@@ -9,14 +9,13 @@ SELECT t.task_id,
        MAX(to_char(DECODE(FIELDCODE, 'TYPE', f.value))) AS DOCTYPE,
        MAX(to_char(DECODE(FIELDCODE, 'CODE', f.value))) AS REGION,
        t.regioncode
-  FROM intask t,
-       intaskrequestfield f
-WHERE t.SERVICECODE = 'R001'
+  FROM itask t,
+       itaskrfield f
+WHERE t.SERVICECODE = '001'
   AND f.TASK_ID = t.TASK_ID
-  AND t.laststatecode = 'I020'
+  AND t.laststatecode = '020'
   AND t.createddt >= :date_from
   AND t.createddt <= :date_to
   AND t.regioncode = :region_code
---  AND t.task_id in ('xxxxxxxxxx')
 GROUP BY t.task_id, t.regioncode
 ORDER BY t.task_id, t.regioncode;
